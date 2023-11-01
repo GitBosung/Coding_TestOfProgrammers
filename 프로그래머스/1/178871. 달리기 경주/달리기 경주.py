f4d549e2961ed7604a9601f788575c1cef849571
GitBosung <37541498+GitBosung@users.noreply.github.com>
2023-11-01 17:23:30 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,15 @@
+def solution(players, callings):
+    player_dict = {player: idx for idx, player in enumerate(players)}
+    
+    for call in callings:
+        idx = player_dict[call]
+        
+        temp = players[idx]
+        players[idx]= players[idx-1]
+        players[idx-1] = temp
+        
+        player_dict[players[idx]] = idx
+        player_dict[players[idx-1]] = idx-1
+        
+    return players
+        
